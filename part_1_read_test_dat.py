96cc4d5fa3841fe8dbbfc06da1a01575f3d0d036
Zoe-2001 <yuchenda@andrew.cmu.edu>
2023-09-24 03:18:08 -0400

    Finished Loading DAT files

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,8 +1,13 @@
-import cc_dat_utils
+import cc_dat_utils as cc
 
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
 
 
-#Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
-#print the resulting data
+
+if __name__ == "__main__":
+    # Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
+    output_cc_level_object = cc.make_cc_level_pack_from_dat(input_dat_file)
+
+    # print the resulting data
+    print(output_cc_level_object)
